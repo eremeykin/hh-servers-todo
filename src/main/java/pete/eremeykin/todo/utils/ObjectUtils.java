a24469d67bfa3f7640e9424e3d4d6607b9c34d77
Petr Eremeykin <eremeykin@gmail.com>
2019-02-22 13:33:23 +0300

    Disable FAIL_ON_EMPTY_BEANS

--- src/main/java/pete/eremeykin/todo/utils/ObjectUtils.java
+++ src/main/java/pete/eremeykin/todo/utils/ObjectUtils.java
@@ -2,6 +2,7 @@ package pete.eremeykin.todo.utils;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 import java.util.Map;
 
@@ -9,6 +10,7 @@ public class ObjectUtils {
 
   public static Map<String, Object> transformToMap(Object object) {
     ObjectMapper mapper = new ObjectMapper();
+    mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
     Map<String, Object> map =
         mapper.convertValue(object, new TypeReference<Map<String, Object>>() {
         });
